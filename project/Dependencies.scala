e5a15bef91ecc90f13feb8fc8cf6740ba9ee3dc7
Scala Steward <me@scala-steward.org>
2021-02-24 14:38:53 +0100

    Update meow-mtl-core, meow-mtl-effects to 0.4.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
 
     val cats          = "2.1.1"
     val catsEffect    = "2.1.2"
-    val catsMeowMtl   = "0.4.0"
+    val catsMeowMtl   = "0.4.1"
     val catsRetry     = "1.1.0"
     val circe         = "0.13.0"
     val ciris         = "1.0.4"
