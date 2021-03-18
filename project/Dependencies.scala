74fad296bc51da938410b532f7949e23980df7d7
Scala Steward <me@scala-steward.org>
2021-03-18 20:27:46 +0100

    Update monocle-core, monocle-macro to 2.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -23,7 +23,7 @@ object Dependencies {
     val refined       = "0.9.21"
     val redis4cats    = "0.9.6"
     val squants       = "1.6.0"
-    val monocle       = "2.0.5"
+    val monocle       = "2.1.0"
     val tapir         = "0.17.19"
 
     // db
