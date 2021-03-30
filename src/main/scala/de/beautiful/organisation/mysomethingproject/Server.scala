c538e70f44e714fde80d4df5a7234d3a95cca547
Scala Steward <me@scala-steward.org>
2021-03-30 04:46:00 +0200

    Applied Scalafix rule(s) github:typelevel/cats/Cats_v2_2_0?sha=v2.2.0
    See https://github.com/typelevel/cats/blob/v2.2.0/scalafix/README.md#migration-to-cats-v220 for details

--- src/main/scala/de/beautiful/organisation/mysomethingproject/Server.scala
+++ src/main/scala/de/beautiful/organisation/mysomethingproject/Server.scala
@@ -1,7 +1,6 @@
 package de.beautiful.organisation.mysomethingproject
 
 import cats.effect._
-import cats.implicits._
 import org.http4s.HttpRoutes
 import org.http4s.dsl.io._
 import org.http4s.implicits._
