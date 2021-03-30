b392e4d21e2ebd6b9b6b4c547fea2ae8cdf997ca
Scala Steward <me@scala-steward.org>
2021-03-30 04:44:49 +0200

    Update refined, refined-cats to 0.9.22

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -20,7 +20,7 @@ object Dependencies {
 
     val log4cats      = "1.1.1"
     val newtype       = "0.4.4"
-    val refined       = "0.9.21"
+    val refined       = "0.9.22"
     val redis4cats    = "0.9.6"
     val squants       = "1.6.0"
     val monocle       = "2.0.5"
